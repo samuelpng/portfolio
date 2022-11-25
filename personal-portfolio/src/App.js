676848a1c133bc3b3e7276c7835906d5ce47a594
Samuel Png <samuelpng7@gmail.com>
2022-11-25 02:59:24 +0000

    added navbar and changed bg color

---
+++ personal-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Fragment>
+      <NavBar />
+      
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ personal-portfolio/src/pages/AboutMe.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+import './Pages.css'
+
+export default function AboutMe() {
+    return (
+        <>
+            <div className = "sean">
+                
+            </div>
+        </>
+    )
+}
